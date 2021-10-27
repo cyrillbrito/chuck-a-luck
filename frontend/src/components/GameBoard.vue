e8103742d69ada44f3be6b856bfa46dcbff5ecd6
Cyrill Brito <cyrill.brito@gmail.com>
2021-10-27 15:53:25 +0100

    Clean up some functions

--- frontend/src/components/GameBoard.vue
+++ frontend/src/components/GameBoard.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="game-board">
+  <div>Ola</div>
+  <div
+    class="game-board"
+    v-bind:class="{
+      locked: noConnection || connectionProblem || generalProblem,
+    }"
+  >
     <div class="tiles">
       <div
         class="tile"
@@ -53,68 +59,41 @@ import Dices from "./Dices.vue";
 })
 export default class GameBoard extends Vue {
   useraccount = "";
-  waves: any[] = [];
+
   selected = 0;
   amount = "";
+
   beingMined = false;
   roll0 = 0;
   roll1 = 0;
   roll2 = 0;
   prize = -1;
 
-  contractAddess = '0xebB74Ec6615a968B355345b45Fd8F2cFB98A8f6E';
+  noConnection = false;
+  connectionProblem = false;
+  generalProblem = false;
 
   mounted() {
     this.checkIfWalletIsConnected();
   }
 
   async checkIfWalletIsConnected(): Promise<void> {
-    try {
-      const { ethereum } = window as any;
-
-      if (!ethereum) {
-        console.log("Make sure you have metamask!");
-        return;
-      } else {
-        console.log("We have the ethereum object", ethereum);
-      }
-
-      /*
-       * Check if we're authorized to access the user's wallet
-       */
-      const accounts = await ethereum.request({ method: "eth_accounts" });
-
-      if (accounts.length !== 0) {
-        const account = accounts[0];
-        console.log("Found an authorized account:", account);
-        this.useraccount = account;
-        // await this.getAllWaves();
-      } else {
+    await this.ethConnect(async (ethereum) => {
+      const accounts = await ethereum.request({
+        method: "eth_accounts",
+      });
+      if (!accounts.length) {
         console.log("No authorized account found");
       }
-    } catch (error) {
-      console.log(error);
-    }
+    });
   }
 
   async connectWallet(): Promise<void> {
-    try {
-      const { ethereum } = window as any;
-
-      if (!ethereum) {
-        alert("Get MetaMask!");
-        return;
-      }
-
-      const accounts = await ethereum.request({
+    await this.ethConnect(async (ethereum) => {
+      await ethereum.request({
         method: "eth_requestAccounts",
       });
-
-      console.log("Connected", accounts[0]);
-      this.useraccount = accounts[0];
-    } catch (error) {
-      console.log(error);
-    }
+    });
   }
 
   async placeBet(): Promise<void> {
@@ -123,108 +102,76 @@ export default class GameBoard extends Vue {
     this.roll1 = 0;
     this.roll2 = 0;
 
-    try {
-      const { ethereum } = window as any;
+    await this.ethCall(async (luckContract) => {
+      const transaction = await luckContract.bet(this.selected - 1, {
+        value: ethers.utils.parseEther(this.amount.toString()),
+        gasLimit: 300000,
+      });
 
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
-        const wavePortalContract = new ethers.Contract(
-          this.contractAddess,
-          contract.abi,
-          signer
-        );
+      const transactionResult = await transaction.wait();
 
-        const transaction = await wavePortalContract.bet(this.selected - 1, {
-          value: ethers.utils.parseEther(this.amount.toString()),
-          gasLimit: 300000,
-        });
+      const [roll0, roll1, roll2, prize] = transactionResult.events[0].args;
+      this.roll0 = roll0 + 1;
+      this.roll1 = roll1 + 1;
+      this.roll2 = roll2 + 1;
+      this.prize = Number(ethers.utils.formatEther(prize));
 
-        const transactionResult = await transaction.wait();
+      console.log(roll0, roll1, roll2, prize);
+      console.log(this.prize);
+    });
 
-        const [roll0, roll1, roll2, prize] = transactionResult.events[0].args;
-        this.roll0 = roll0 + 1;
-        this.roll1 = roll1 + 1;
-        this.roll2 = roll2 + 1;
-        this.prize = Number(ethers.utils.formatEther(prize));
-
-        console.log(roll0, roll1, roll2, prize);
-        console.log(ethers.utils.formatEther(prize));
-
-        this.beingMined = false;
-      } else {
-        this.beingMined = false;
-        alert("Ethereum object doesn't exist!");
-      }
-    } catch (error) {
-      this.beingMined = false;
-      alert("Oops! Somethings went wrong.");
-      console.error(error);
-    }
-  }
-
-  async count(): Promise<void> {
-    try {
-      const { ethereum } = window as any;
-
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
-        const wavePortalContract = new ethers.Contract(
-          this.contractAddess,
-          contract.abi,
-          signer
-        );
-
-        let count = await wavePortalContract.getTotalPings();
-        console.log("Retrieved total wave count...", count.toNumber());
-      } else {
-        console.log("Ethereum object doesn't exist!");
-      }
-    } catch (error) {
-      console.log(error);
-    }
+    this.beingMined = false;
   }
 
   async getAllWaves(): Promise<void> {
+    this.ethCall(async (luckContract) => {
+      const waves = await luckContract.getAllWaves();
+      const waves2: any[] = [];
+      waves.forEach((wave: any) => {
+        waves2.push({
+          address: wave.waver,
+          timestamp: new Date(wave.timestamp * 1000),
+          message: wave.message,
+        });
+      });
+
+      console.log(waves2);
+    });
+  }
+
+  private async ethConnect(fn: (ethereum: any) => Promise<void>) {
     try {
       const { ethereum } = window as any;
-      if (ethereum) {
+
+      if (!ethereum) {
+        console.error("Ethereum object doesn't exist!");
+        this.connectionProblem = true;
+        return;
+      }
+
+      await fn(ethereum);
+    } catch (error) {
+      console.error(error);
+      this.connectionProblem = true;
+    }
+  }
+
+  private async ethCall(fn: (contract: ethers.Contract) => Promise<void>) {
+    try {
+      this.ethConnect(async (ethereum) => {
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
-        const wavePortalContract = new ethers.Contract(
-          this.contractAddess,
+        const luckContract = new ethers.Contract(
+          "0xebB74Ec6615a968B355345b45Fd8F2cFB98A8f6E",
           contract.abi,
           signer
         );
 
-        /*
-         * Call the getAllWaves method from your Smart Contract
-         */
-        const waves = await wavePortalContract.getAllWaves();
-
-        /*
-         * We only need address, timestamp, and message in our UI so let's
-         * pick those out
-         */
-        waves.forEach((wave: any) => {
-          this.waves.push({
-            address: wave.waver,
-            timestamp: new Date(wave.timestamp * 1000),
-            message: wave.message,
-          });
-        });
-
-        /*
-         * Store our data in React State
-         */
-        // this.waves = wavesCleaned;
-        console.log(this.waves);
-      } else {
-        console.log("Ethereum object doesn't exist!");
-      }
+        await fn(luckContract);
+      });
     } catch (error) {
-      console.log(error);
+      console.error(error);
+      this.generalProblem = true;
     }
   }
 }
@@ -249,7 +196,7 @@ export default class GameBoard extends Vue {
 .tile:not(:nth-child(3n)) {
   border-right: 1px solid #f2e5bd;
 }
-.tile:not(:nth-child(n+4)){
+.tile:not(:nth-child(n + 4)) {
   border-bottom: 1px solid #f2e5bd;
 }
 .tile div {
